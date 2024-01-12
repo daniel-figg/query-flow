6fc32fb3aa7f1f4b14b3eec56d0f9990852eb58c
daniel-figg <daniel.figg05@gmail.com>
2024-01-11 23:20:27 -0500

    add db user check

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,13 +1,23 @@
 import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
 import { redirect } from "next/navigation";
+import Dashboard from "~/components/Dashboard";
+import { db } from "~/server/db";
 
 const Page = async () => {
   const { getUser } = getKindeServerSession();
   const user = await getUser();
 
-  if (!user || !user.id) redirect("/auth-callback?origin=dashboard");
+  if (!user?.id) redirect("/auth-callback?origin=dashboard");
 
-  return <div>{user?.email}</div>;
+  const dbUser = await db.user.findFirst({
+    where: {
+      id: user.id,
+    },
+  });
+
+  if (!dbUser) redirect("/auth-callback?origin=dashboard");
+
+  return <Dashboard />;
 };
 
 export default Page;
