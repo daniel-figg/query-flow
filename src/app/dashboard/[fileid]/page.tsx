0b559e69fe01b479eb35a968205a8a671a89c324
daniel-figg <daniel.figg05@gmail.com>
2024-01-12 18:54:33 -0500

    setup individual file view

---
+++ src/app/dashboard/[fileid]/page.tsx
@@ -0,0 +1,49 @@
+import { getKindeServerSession } from "@kinde-oss/kinde-auth-nextjs/server";
+import { notFound, redirect } from "next/navigation";
+import PdfRenderer from "~/components/PdfRenderer";
+import ChatWrapper from "~/components/chat/ChatWrapper";
+import { db } from "~/server/db";
+
+interface PageProps {
+  params: {
+    fileid: string;
+  };
+}
+
+const Page = async ({ params }: PageProps) => {
+  const { fileid } = params;
+
+  const { getUser } = getKindeServerSession();
+  const user = await getUser();
+
+  if (!user?.id) redirect(`/auth-callback?origin=dashboard/${fileid}`);
+
+  const file = await db.file.findFirst({
+    where: {
+      id: fileid,
+      userId: user.id,
+    },
+  });
+
+  if (!file) notFound();
+
+  return (
+    <div className="flex h-[calc(100vh-3.5rem)] flex-1 flex-col justify-between">
+      <div className="max-w-8xl mx-auto w-full grow lg:flex xl:px-2">
+        {/* Left sidebar & main wrapper */}
+        <div className="flex-1 xl:flex">
+          <div className="px-4 py-6 sm:px-6 lg:pl-8 xl:flex-1 xl:pl-6">
+            {/* Main area */}
+            <PdfRenderer />
+          </div>
+        </div>
+
+        <div className="flex-[0.75] shrink-0 border-t border-gray-200 lg:w-96 lg:border-l lg:border-t-0">
+          <ChatWrapper />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
