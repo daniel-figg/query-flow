4ec596e68c5f206f0d96a326d2b6c76cc3537b95
daniel-figg <daniel.figg05@gmail.com>
2024-01-12 21:27:30 -0500

    get file route

--- src/server/api/routers/file.ts
+++ src/server/api/routers/file.ts
@@ -13,6 +13,23 @@ export const fileRouter = createTRPCRouter({
       },
     });
   }),
+
+  getFile: privateProcedure
+    .input(z.object({ key: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      const { userId } = ctx;
+
+      const file = await db.file.findFirst({
+        where: {
+          key: input.key,
+          userId,
+        },
+      });
+
+      if (!file) throw new TRPCError({ code: "NOT_FOUND" });
+
+      return file;
+    }),
   deleteFile: privateProcedure
     .input(z.object({ id: z.string() }))
     .mutation(async ({ ctx, input }) => {
