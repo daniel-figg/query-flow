7303538dbb27f9287fdd0e11a69aa6d136694ae5
daniel-figg <daniel.figg05@gmail.com>
2024-01-13 18:02:02 -0500

    add functionality

--- src/components/chat/ChatWrapper.tsx
+++ src/components/chat/ChatWrapper.tsx
@@ -1,5 +1,95 @@
-const ChatWrapper = () => {
-  return <div>ChatWrapper</div>;
+"use client";
+
+import ChatInput from "./ChatInput";
+import Messages from "./Messages";
+import { ChevronLeft, Loader2, XCircle } from "lucide-react";
+import Link from "next/link";
+import { buttonVariants } from "../ui/button";
+import { api } from "~/trpc/react";
+
+interface ChatWrapperProps {
+  fileId: string;
+}
+
+const ChatWrapper = ({ fileId }: ChatWrapperProps) => {
+  const { data, isLoading } = api.chat.getFileUploadStatus.useQuery(
+    {
+      fileId,
+    },
+    {
+      refetchInterval: (data) =>
+        data?.status === "SUCCESS" || data?.status === "FAILED" ? false : 500,
+    },
+  );
+
+  if (isLoading)
+    return (
+      <div className="relative flex min-h-full flex-col justify-between gap-2 divide-y divide-zinc-200 bg-zinc-50">
+        <div className="mb-28 flex flex-1 flex-col items-center justify-center">
+          <div className="flex flex-col items-center gap-2">
+            <Loader2 className="h-8 w-8 animate-spin text-blue-500" />
+            <h3 className="text-xl font-semibold">Loading...</h3>
+            <p className="text-sm text-zinc-500">
+              We&apos;re preparing your PDF.
+            </p>
+          </div>
+        </div>
+
+        <ChatInput isDisabled />
+      </div>
+    );
+
+  if (data?.status === "PROCESSING")
+    return (
+      <div className="relative flex min-h-full flex-col justify-between gap-2 divide-y divide-zinc-200 bg-zinc-50">
+        <div className="mb-28 flex flex-1 flex-col items-center justify-center">
+          <div className="flex flex-col items-center gap-2">
+            <Loader2 className="h-8 w-8 animate-spin text-blue-500" />
+            <h3 className="text-xl font-semibold">Processing PDF...</h3>
+            <p className="text-sm text-zinc-500">This won&apos;t take long.</p>
+          </div>
+        </div>
+
+        <ChatInput isDisabled />
+      </div>
+    );
+
+  if (data?.status === "FAILED")
+    return (
+      <div className="relative flex min-h-full flex-col justify-between gap-2 divide-y divide-zinc-200 bg-zinc-50">
+        <div className="mb-28 flex flex-1 flex-col items-center justify-center">
+          <div className="flex flex-col items-center gap-2">
+            <XCircle className="h-8 w-8 text-red-500" />
+            <h3 className="text-xl font-semibold">Too many pages in PDF</h3>
+            <p className="text-sm text-zinc-500">
+              Your plan supports up to 5 pages per PDF.
+            </p>
+            <Link
+              href="/dashboard"
+              className={buttonVariants({
+                variant: "secondary",
+                className: "mt-4",
+              })}
+            >
+              <ChevronLeft className="mr-1.5 h-3 w-3" />
+              Back
+            </Link>
+          </div>
+        </div>
+
+        <ChatInput isDisabled />
+      </div>
+    );
+
+  return (
+    <div className="relative flex min-h-full flex-col justify-between gap-2 divide-y divide-zinc-200 bg-zinc-50">
+      <div className="mb-28 flex flex-1 flex-col justify-between">
+        <Messages fileId={fileId} />
+      </div>
+
+      <ChatInput />
+    </div>
+  );
 };
 
 export default ChatWrapper;
