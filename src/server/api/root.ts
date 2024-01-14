5bc6ee0f037647a327c2ff165a65841f4897c895
daniel-figg <daniel.figg05@gmail.com>
2024-01-14 17:30:25 -0500

    add stripe router

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -2,6 +2,7 @@ import { authRouter } from "~/server/api/routers/auth";
 import { createTRPCRouter } from "~/server/api/trpc";
 import { fileRouter } from "./routers/file";
 import { chatRouter } from "./routers/chat";
+import { stripeRouter } from "./routers/stripe";
 
 /**
  * This is the primary router for your server.
@@ -12,6 +13,7 @@ export const appRouter = createTRPCRouter({
   auth: authRouter,
   file: fileRouter,
   chat: chatRouter,
+  stripe: stripeRouter,
 });
 
 // export type definition of API
