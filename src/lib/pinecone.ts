6f493d80c7cb05928d8d30d6a4c660e65132d940
daniel-figg <daniel.figg05@gmail.com>
2024-01-13 22:34:26 -0500

    setup pinecone

---
+++ src/lib/pinecone.ts
@@ -0,0 +1,7 @@
+// v1.0.0 initialization
+import { Pinecone } from "@pinecone-database/pinecone";
+
+export const pinecone = new Pinecone({
+  apiKey: process.env.PINECONE_API_KEY!,
+  environment: "gcp-starter",
+});
