f98fc2bbc4a7197ace3b80a7af1540e23b0abfb4
daniel-figg <daniel.figg05@gmail.com>
2024-01-15 01:15:36 -0500

    add metadata

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,4 +1,5 @@
 import { type ClassValue, clsx } from "clsx";
+import { Metadata } from "next";
 import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
@@ -10,4 +11,47 @@ export function absoluteUrl(path: string) {
   if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}${path}`;
 
   return `http://localhost:${process.env.PORT ?? 3000}${path}`;
+}
+
+export function constructMetadata({
+  title = "QueryFlow - the SaaS for students",
+  description = "QueryFlow is an open-source software to make chatting to your PDF files easy.",
+  image = "/thumbnail.png",
+  icons = [{ rel: "icon", url: "/favicon.ico" }],
+  noIndex = false,
+}: {
+  title?: string;
+  description?: string;
+  image?: string;
+  icons?: { rel: string; url: string }[];
+  noIndex?: boolean;
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [
+        {
+          url: image,
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images: [image],
+      creator: "@joshtriedcoding",
+    },
+    icons,
+    metadataBase: new URL("https://query-flow-chi.vercel.app"),
+    ...(noIndex && {
+      robots: {
+        index: false,
+        follow: false,
+      },
+    }),
+  };
 }
