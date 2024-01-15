2d13784364951d242c5ac34b3d429e8dc8b22a06
daniel-figg <daniel.figg05@gmail.com>
2024-01-15 13:28:21 -0500

    fix styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -144,7 +144,7 @@ export default function Home() {
 
         <div className="mx-auto max-w-6xl px-6 lg:px-8">
           <div className="mt-16 flow-root sm:mt-24">
-            <div className="-m-2 rounded-xl bg-gray-900/5 p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
+            <div className="-m-2 rounded-xl bg-muted p-2 ring-1 ring-inset ring-gray-900/10 lg:-m-4 lg:rounded-2xl lg:p-4">
               <Image
                 src="/file-upload-preview.jpg"
                 alt="uploading preview"
